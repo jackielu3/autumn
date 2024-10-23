c46ba0c1e415e9a6ffb41017da2234577f0e9fd3
MossyDartGames <andrewjube02@gmail.com>
2024-10-23 12:48:45 -0700

    This Camera will kill me someday

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,14 +6,42 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] Transform followTarget;
 
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] float bottomClamp = -40f;
+    [SerializeField] float topClamp = 70f;
+
+    private float cinemachineTargetPitch;
+    private float cinemachineTargetYaw;
+
+
+    private void LateUpdate()
+    {
+        cameraLogic();
+    }
+    private void cameraLogic()
+    {
+        float mouseX = GetMouseInput("Mouse X");
+        float mouseY = GetMouseInput("Mouse Y");
+        cinemachineTargetPitch = updateRotation(cinemachineTargetPitch, mouseY, bottomClamp, topClamp, true);
+        cinemachineTargetYaw = updateRotation(cinemachineTargetYaw, mouseX, float.MinValue, float.MaxValue, false);
+        applyRotation(cinemachineTargetPitch, cinemachineTargetYaw);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void applyRotation(float pitch, float yaw)
     {
+        followTarget.rotation = Quaternion.Euler(pitch, yaw, followTarget.eulerAngles.z);
+    }
+
+    private float updateRotation(float currentRotation, float input, float min, float max, bool isXAxis)
+    {
+        currentRotation += isXAxis ? -input : input;
+        return Mathf.Clamp(currentRotation, min, max);
+    }
+
+    private float GetMouseInput(string axis)
+    { 
+        return Input.GetAxis(axis) * rotationSpeed *Time.deltaTime; 
     }
 }
