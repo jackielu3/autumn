eb0d74d24acdc6bd9c97b081eb85c1bcf6d1c785
Jackie Lu <jackielu3@gmail.com>
2024-11-19 20:50:40 -0800

    data persistance init

---
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int uwu;
+
+    // Initial starting values (when you create a new game uwu)
+    public GameData()
+    {
+        this.uwu = 0;
+    }
+}
