95fe6f4267f8eba6b3648c917f45b05dd4ef852e
Jackie Lu <jackielu3@gmail.com>
2025-06-02 22:43:11 -0700

    init

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
 public class GameData
 {
-    public int uwu;
-
+    int uwu = 0;
     // Initial starting values (when you create a new game uwu)
     public GameData()
     {
