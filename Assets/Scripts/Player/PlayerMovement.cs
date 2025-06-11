2d108c406704a8e76abb61c483b3df4f79c56cee
jackielu3 <jackielu3@gmail.com>
2025-06-10 22:37:25 -0700

    player movement updates

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,151 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject model;
+
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 3.5f;
+    [SerializeField] private float sprintSpeed = 6.0f;
+    [SerializeField] private float dashForce = 5.0f;
     [SerializeField][ReadOnly] private float currentSpeed;
-
-    [Header("General")]
-    [SerializeField][ReadOnly] private Vector3 move;
-
-    [Header("Grounded")]
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private float speed = 3.5f;
-    [SerializeField] private float dashDistance = 5.0f;
+    [SerializeField][ReadOnly] private Vector3 moveInput;
+    [SerializeField][ReadOnly] private Vector3 moveVelocity;
     [SerializeField] private bool isSprinting;
 
     [Header("Jump")]
     [SerializeField] private float jumpForce = 5f;
-    [SerializeField] public float groundDistance = 0.5f;
+    [SerializeField] private float groundDistance = 0.5f;
+    [SerializeField][ReadOnly] private bool isGrounded;
 
-    [Header("Animations")]
-    [SerializeField] private Animator animator;
-    [SerializeField] private GameObject model;
 
-    void Awake()
+    private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
     }
 
-    bool IsGrounded()
-    {
-        return Physics.Raycast(transform.position, Vector3.down, groundDistance);
-    }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        GetMove();
-        isGrounded = IsGrounded();
-        
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            isSprinting = true;
-            speed = 6.0f;
-            Sprinting();
- 
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            IsDashing();
-        }
-        else
-        {
-            isSprinting = false;
-            speed = 3.5f;
-            Walking();
-        }
-        
-        animator.SetBool("Grounded", isGrounded);
-        animator.SetFloat("Speed", currentSpeed);
-
-
-
-        if (Input.GetAxis("Mouse X") < 0)
-        {
-            //Code for action on mouse moving left
-            // print("Mouse moved left");
-        }
-        if (Input.GetAxis("Mouse X") > 0)
-        { 
-            //Code for action on mouse moving right
-            // print("Mouse moved right");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rigidBody.velocity = Vector3.up * jumpForce;
-            animator.SetBool("Jump", true);
-        }
-        else
-        {
-            if (isGrounded)
-            {
-                animator.SetBool("Jump", false);
-            }
-        }
+        HandleInput();
+        HandleJump();
+        UpdateAnimator();
     }
 
-    void Walking()
+    // Frame-rate independent update
+    private void FixedUpdate()
     {
-        // Apply movement
-        if (move != Vector3.zero)
-        {
-            model.transform.forward = move.normalized;
-            transform.position += move;
-        }
+        HandleMovement();
     }
 
-    void Sprinting()
-    {
-        // Apply movement
-        if (move != Vector3.zero)
-        {
-            model.transform.forward = move.normalized;
-            transform.position += move;
-        }
-    }
-
-    void OnLand()
-    {
-        // Used for Animator, will hopefully implement special conditions for landing later
-    }
-
-    void OnFootstep()
-    {
-        // Used for Animator
-    }
-
-
-
-    Vector3 GetMove() 
+    // Handles all inputs that directly affect the player's movement (except jump)
+    private void HandleInput()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        var targetAngle = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
-
-
-        currentSpeed = new Vector3(horizontal, 0, vertical).normalized.magnitude * speed;
-        
-
-        // Get the camera's forward and right vectors, ignoring any vertical direction
         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
         Vector3 cameraRight = Vector3.Scale(Camera.main.transform.right, new Vector3(1, 0, 1)).normalized;
 
-        // Calculate the movement direction based on input and camera's orientation
-        move = (cameraForward * vertical + cameraRight * horizontal).normalized * currentSpeed * Time.deltaTime;
+        moveInput = (cameraForward * vertical + cameraRight * horizontal).normalized;
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance);
 
-        return move;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Dash();
+        }
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+        float speed = isSprinting ? sprintSpeed : walkSpeed;
+        moveVelocity = moveInput * speed;
+        currentSpeed = moveVelocity.magnitude;
     }
 
-    // Temporary Dash function, to be removed upon completion of the smoother dash
-    void IsDashing()
+    // Handles all logic revolving around jumping
+    private void HandleJump()
     {
-        transform.position += GetMove().normalized * dashDistance;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, jumpForce, rigidBody.velocity.z);
+            animator.SetBool("Jump", true);
+        }
+        else if (isGrounded)
+        {
+            animator.SetBool("Jump", false);
+        }
     }
+
+    // TODO A very simple dash, need to make more robust
+    private void Dash()
+    {
+        if (moveInput == Vector3.zero) return;
+
+        Vector3 dashDirection = moveInput.normalized;
+        rigidBody.AddForce(dashDirection * dashForce, ForceMode.VelocityChange);
+    }
+
+    // Handles rigidbody movement based on user input
+    private void HandleMovement()
+    {
+        // Only update horizontal velocity
+        Vector3 horizontalVelocity = moveVelocity;
+        horizontalVelocity.y = rigidBody.velocity.y;
+
+        rigidBody.velocity = horizontalVelocity;
+
+        // Rotate model toward movement
+        if (moveInput != Vector3.zero)
+        {
+            model.transform.forward = moveInput;
+        }
+    }
+
+    // State updates for the animator
+    private void UpdateAnimator()
+    {
+        animator.SetBool("Grounded", isGrounded);
+        animator.SetFloat("Speed", currentSpeed);
+    }
+
+
+    // TODO Implement!!
+    private void OnLand() { }
+    private void OnFootstep() { }
+
 }
+
+
+
